cced85d6b8183dac23ffecd7f032a857e311d139
valentsk <skredovavalenti@mail.ru>
2022-11-13 16:32:35 +0300

    Resolve task with method find new size for 3 symbols

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿Console.Clear();
 
 Console.Write("Введи размерность массива: ");
-string[] array = CreateArray();
+string[] arrayInit = CreateArray();
 
 Console.WriteLine("Введите символы, строки через Enter для заполнения массива:");
-FillArray(array);
+FillArray(arrayInit);
+Console.Write("Начальный массив:");
+PrintArray(arrayInit);
+int newSize = CheckNewSize(arrayInit);
+string[] arrayFinal = CreateArrayThreeSymbols(newSize);
+FillArrayThreeSymbols(arrayInit, arrayFinal);
 Console.Write("Конечный массив:");
-PrintArray(array);
+PrintArray(arrayFinal);
 
 
 string[] CreateArray()
@@ -26,6 +31,37 @@ void FillArray(string[] array)
 
 void PrintArray(string[] array)
 {
-    Console.WriteLine("[" + string.Join(", ", array) + "]");
+    Console.WriteLine("['" + string.Join("', '", array) + "']");
 }
 
+int CheckNewSize(string[] array)
+{
+    int newSize = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newSize++;
+        }
+    }
+    return newSize;
+}
+
+string[] CreateArrayThreeSymbols(int size)
+{
+    string[] array = new string[size];
+    return array;
+}
+
+void FillArrayThreeSymbols(string[] arrayStart, string[] arrayFinal)
+{
+    int j = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+        if (arrayStart[i].Length <= 3)
+        {
+            arrayFinal[j] = arrayStart[i];
+            j++;
+        }
+    }
+}
